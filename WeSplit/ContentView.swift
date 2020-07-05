a1a57707248f5cac4d32caed699cc2e3cf371fb3
Rami Alhussein <Ramialhussein98@gmail.com>
2020-07-05 16:04:55 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Rami Alhussein on 7/5/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
